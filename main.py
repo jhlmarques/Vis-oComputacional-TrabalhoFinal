b2ad22497b33175223954895d86b60cf69c5a4e5
JHLM <josehenriquelm@gmail.com>
2023-03-28 10:43:44 -0300

    Adds command line image input

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import cv2 as cv
 import numpy as np
 import os
+import argparse
 
 class CoffeeDetector:
     def __init__(self) -> None:
@@ -22,4 +23,20 @@ class CoffeeDetector:
 
 
 if __name__ == '__main__':
-    pass
+    allowed_extensions = ['.jpg', '.png']
+    
+    # Obtém imagem via linha de comando
+    parser = argparse.ArgumentParser(
+        prog = 'CoffeeVision',
+        description= 'Detecta quantidade de café contido em cafeteira capturada em imagem'
+    )
+
+    parser.add_argument('filename')
+    args = parser.parse_args()
+    
+    image_filename = args.filename
+    _, ext = os.path.splitext(image_filename)
+    if(ext not in allowed_extensions):
+        raise ValueError('Tipos de extensão de arquivo suportadas: ' + ', '.join(allowed_extensions))
+
+    image = np.fromfile(image_filename, dtype=np.uint8)
