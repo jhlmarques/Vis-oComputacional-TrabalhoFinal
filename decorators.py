714e6e56c51c2beb489a1213f82c2798e8e76bc8
JHLM <josehenriquelm@gmail.com>
2023-04-03 21:52:06 -0300

    Adds obligatory parameter to image output decorator

--- decorators.py
+++ decorators.py
@@ -3,20 +3,22 @@ import cv2 as cv
 '''
 Decorador para mostrar saída das funções
 '''
-def showImageOutput(function, title='Test'):
+def showImageOutput(title):
+    def decorator(function):
+        def wrapper(*args, **kwargs):
+            image = function(*args, **kwargs)
 
-    def wrapper(*args, **kwargs):
-        image = function(*args, **kwargs)
+            if(image.shape[0] > 500 or image.shape[1] > 500):
+                aspect_ratio = image.shape[0]/image.shape[1]
+                resized = cv.resize(image, (500, int(500*aspect_ratio)))
+                cv.imshow(title, resized)
+            else:
+                cv.imshow(title, image)
 
-        if(image.shape[0] > 500 or image.shape[1] > 500):
-            aspect_ratio = image.shape[0]/image.shape[1]
-            resized = cv.resize(image, (500, int(500*aspect_ratio)))
-            cv.imshow(title, resized)
-        else:
-            cv.imshow(title, image)
-
-        cv.waitKey(0)
+            cv.waitKey(0)
+            cv.destroyWindow(title)
         
-        return image
+            return image
+        return wrapper
     
-    return wrapper
+    return decorator
