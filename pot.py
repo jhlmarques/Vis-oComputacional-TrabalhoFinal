31ecbadb658b87e39fbfd76734e155ec6300c810
JHLM <josehenriquelm@gmail.com>
2023-04-07 03:52:19 -0300

    Adds offset to elipse pot

--- pot.py
+++ pot.py
@@ -10,7 +10,7 @@ do eixo perpendicular à base
 @dataclass
 class Pot:
     height_cm : float
-    basis_diameter_cm : float
+    basis_radius_cm : float
 
     def getRadiusAtHeight(self, y: int):
         raise NotImplemented
@@ -27,6 +27,7 @@ class Pot:
         pixel_height = int(im_height * pixel_cm_ratio)
         pixel_center_x = center[0]
         pixel_center_y = center[1]
+        cv.circle(image, (pixel_center_x, pixel_center_y), point_radius+5, (255, 255, 0), -1)
         try:
             for p in range(0, pixel_height - pixel_center_y, step_im):
                 radius = self.getRadiusAtHeight(pot_y)
@@ -35,11 +36,12 @@ class Pot:
                 pixel_x2 = pixel_center_x + pixel_radius
 
                 pixel_y = pixel_center_y - p
-                cv.circle(image, (pixel_center_x, pixel_y), point_radius, (0, 0, 255), -1)
-                cv.circle(image, (pixel_x1, pixel_y), point_radius, (255, 0, 0), -1)
-                cv.circle(image, (pixel_x2, pixel_y), point_radius, (255, 0, 0), -1)
+                cv.line(image, (pixel_x1, pixel_y), (pixel_x2, pixel_y), (0, 0, 255))
+                cv.circle(image, (pixel_x1, pixel_y), point_radius, (0, 255, 0), -1)
+                cv.circle(image, (pixel_x2, pixel_y), point_radius, (0, 255, 0), -1)
 
                 pot_y += step_height
+
         except ValueError: # Y fora da elipse
             pass
 
@@ -53,21 +55,25 @@ https://pt.wikipedia.org/wiki/Elipse
 
 @dataclass
 class PotElliptical(Pot):
+    height_offset_cm : float
     coef1 : float = field(init=False)
     coef2 : float = field(init=False)
 
     def __post_init__(self):
-        self.coef1 = self.basis_diameter_cm
+        self.coef1 = self.basis_radius_cm
         self.coef2 = -(1 / (self.height_cm**2))         
 
     #https://math.stackexchange.com/questions/2195472/calculating-a-points-x-position-on-an-ellipse-given-pos-y
     def getRadiusAtHeight(self, y: int):
-        return self.coef1 * sqrt(1 + self.coef2 * (y**2))
+        if y <= self.height_offset_cm:
+            return self.basis_radius_cm
+        else:
+            return self.coef1 * sqrt(1 + self.coef2 * ((y - self.height_offset_cm)**2))
 
 
 if __name__ == '__main__':
     image = np.zeros((400, 400, 3), dtype=np.uint8)
-    pot = PotElliptical(30, 40)
+    pot = PotElliptical(30, 30, 10)
     pot.drawPoints(image, (200, 300), 4 / 1, 1, 1)
     cv.imshow('Elliptical Pot', image)
     cv.waitKey(0)
